3485a7fd7836f9736ca7768d1e397a363bcb7ee6
DongJu Kim <jook1356@gmail.com>
2023-07-10 15:16:56 +0900

    feat: 메일링 서비스 적용, application.yml 분리

--- server/api-module/src/main/java/com/myapp/api/dto/user/SignUpDto.java
+++ server/api-module/src/main/java/com/myapp/api/dto/user/SignUpDto.java
@@ -6,19 +6,30 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
 public class SignUpDto {
 
-    @NotBlank(message = "4")
-    private String username;
-    @NotBlank(message = "4")
-    private String password;
-    @NotBlank(message = "4")
-    private String checkedPassword;
-    @NotBlank(message = "4")
+    @NotBlank(message = "100")
+    @Pattern(regexp = "^[가-힣]{2,10}$" , message = "105")
     private String name;
+
+    @NotBlank(message = "101")
+    @Pattern(regexp = "^[a-z0-9]{4,20}$", message = "106")
+    private String username;
+
+    @NotBlank(message = "102")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$", message = "107")
+    private String password;
+
+    @NotBlank(message = "103")
+    private String checkedPassword;
+
+    @NotBlank(message = "104")
+    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "109")
+    private String email;
 
     private Role role;
 
@@ -26,6 +37,7 @@ public class SignUpDto {
     public SignUpDto(User user) {
         this.username = user.getUsername();
         this.password = user.getPassword();
+        this.email = user.getEmail();
         this.name = user.getName();
         this.role = Role.USER;
     }
