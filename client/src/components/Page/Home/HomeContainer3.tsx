39b026429fe227b0f69c3ae4444bcbedf8ebf0e1
DongJu Kim <jook1356@gmail.com>
2023-07-27 16:32:51 +0900

    feat: contact phase 완성

--- client/src/components/Page/Home/HomeContainer3.tsx
+++ client/src/components/Page/Home/HomeContainer3.tsx
@@ -4,6 +4,7 @@ import ContainerContent from "../../Interface/Container/ContainerContent";
 import Animator from "../../Interface/Animator/useAnimator";
 import { setConditionType } from "../../Interface/Container/useContainer";
 import useAnimator from "../../Interface/Animator/useAnimator";
+import { CONTACT_ICON, PHONE_ICON } from "@/components/Assets/ContactIcons";
 
 type HomeContainer1Type = {
   setCondition: setConditionType;
@@ -14,38 +15,70 @@ function HomeContainer3({ setCondition, currentStep }: HomeContainer1Type) {
   const condition = setCondition(currentStep);
   const {Animator, render} = useAnimator(condition.immediate)
 
-  const parellelogram = (
+  const circle = (
     <div
       css={Animator.Translate({
         id: "parellel3",
-        trigger: condition.immediate,
+        trigger: condition.maintain,
         duration: 1000,
         delay: 200,
-        offset: ["70vw", "0px"],
-        option: { hasReverse: true },
+        offset: ["0", "-70vw"],
       })}
     >
       <div
-        css={[
-          Animator.Rotate({
-            id: "parellel-inner3",
-            trigger: condition.immediate,
-            duration: 1000,
-            delay: 600,
-            offset: "20deg",
-            option: { hasReverse: true },
-          }),
+        css={
           css`
-            width: 150vw;
-            height: 150vw;
-
+            width: 80vh;
+            height: 80vh;
+            border-radius: 100%;
             /* background: linear-gradient( to right, white, rgba(0, 0, 0, 0.3) ); */
-            background-color: rgba(0, 0, 0, 0.1);
-            position: absolute;
-            right: -100vw;
-            top: -7vw;
+            background-color: #ffffff;
+            /* box-shadow: 0px 0px 100px 0px rgba(255, 222, 185, 0.493);; */
+            /* position: absolute; */
+            
             /* visibility: hidden; */
-          `,
+          `
+        }
+      />
+    </div>
+  );
+
+  const parellelogram = (
+    <div
+      css={[Animator.Translate({
+        id: "page-3-square-translate",
+        trigger: condition.maintain,
+        duration: 30000,
+        delay: 1000,
+        offset: ["0px", "-40vh"],
+        option: {
+          opacityFrom: '100%'
+        }
+      }), css` position: absolute;`]}
+    >
+      <div
+        css={[Animator.Scale({
+          id: "page-3-square-scale",
+          trigger: condition.maintain,
+          duration: 27000,
+          delay: 1000,
+          offset: ["0%", "100%"],
+          option: {
+            opacityFrom: '100%'
+          }
+        }),
+          css`
+            width: 80vh;
+            height: 80vh;
+            z-index: 0;
+            /* background: linear-gradient( to right, white, rgba(0, 0, 0, 0.3) ); */
+            background-color: #ffffff;
+            box-shadow: 0px 0vh 10vh 50px #ffffff;
+            border-radius: 100%;
+            /* top: 0%; */
+            /* top: 100vh; */
+            /* visibility: hidden; */
+          `
         ]}
       />
     </div>
@@ -54,9 +87,18 @@ function HomeContainer3({ setCondition, currentStep }: HomeContainer1Type) {
   return (
     <ContainerContent customCss={containerWrapperCSS}>
       {render && parellelogram}
+      {render && circle}
+      
       <ContainerContent.Inner >
         <div css={innerContentWrapperCSS}>
-          <div
+          <div css={[Animator.Translate({
+                id: "contact-title",
+                trigger: condition.maintain,
+                duration: 1000,
+                delay: 100,
+                offset: ["0px", "100px"],
+              }), contactTextWrapperCSS]}>Contact</div>
+              <div
             css={[
               Animator.Translate({
                 id: "label",
@@ -66,12 +108,16 @@ function HomeContainer3({ setCondition, currentStep }: HomeContainer1Type) {
                 offset: ["0px", "100px"],
               }),
               css`
-                font-size: 5vw;
+                font-size: 2vw;
+                color: rgba(0, 0, 0, 0.6);
+                margin-bottom: 24px;
               `,
             ]}
           >
-            Hi!
+            열린 마음으로 경청하겠습니다.
           </div>
+            
+
           <div
             css={[
               Animator.Translate({
@@ -83,10 +129,20 @@ function HomeContainer3({ setCondition, currentStep }: HomeContainer1Type) {
               }),
               css`
                 font-size: 5vw;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                width: 100%;
+                justify-content: center;
+                margin-bottom: 140px;
+                & path {
+                  stroke: rgba(0, 0, 0, 0.6);
+                }
               `,
             ]}
           >
-            I'm Frontend Developer!
+            {CONTACT_ICON}
+            <div css={css`margin-top: -100px; width: 20%; height: 20px; border-radius: 100%; box-shadow: 0px 90px 30px 0px rgba(0, 0, 0, 0.2);`}></div>
           </div>
           <div
             css={[
@@ -98,14 +154,30 @@ function HomeContainer3({ setCondition, currentStep }: HomeContainer1Type) {
                 offset: ["0px", "100px"],
               }),
               css`
-                font-size: 2vw;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                width: 100%;
+                /* margin-top: 0px; */
+                
+
               `,
             ]}
           >
-            But I know how to handle the backend, CI/CD as well!
+            <div css={css`display: flex; flex-direction:column; align-items: center;`}>
+              
+            <div css={css`font-size: 24px; font-weight: 700; margin-bottom: 16px;`}>김동주</div>
+              <div css={css`color: rgba(0, 0, 0, 0.6);`}>+82 10-8521-6414</div>
+              <div css={css`color: rgba(0, 0, 0, 0.6);`}>jook1356@gmail.com</div>
+            
+            </div>
+            
+ 
+            
           </div>
+
         </div>
-        <div></div>
+      
       </ContainerContent.Inner>
     </ContainerContent>
   );
@@ -114,14 +186,20 @@ function HomeContainer3({ setCondition, currentStep }: HomeContainer1Type) {
 const containerWrapperCSS = css`
   background-color: #e1ff00;
   overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   /* opacity: 70%; */
 `;
 
 const innerContentWrapperCSS = css`
-  height: 70%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
 `;
+
+const contactTextWrapperCSS = css`font-size: 5vw; font-weight: 700; `
 
 export default HomeContainer3;
